71a315ea872a9f44b83764d3ada34f28cf50343d
Serch24-Hub <sergio_lozano2002@hotmail.com>
2025-04-30 16:35:39 -0400

    Lab dice #1 (Solved)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 let dice = {
   sides: 6,
   roll: function () {
-  let randomNumber;
+  //let randomNumber;
   // Write Code Here
   // Generate a random number between 1 and the number of sides and assign it to randomNumber
+  let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  return (randomNumber);
 
    return (randomNumber);
-  }
+  },
 };
 
 function printNumber(number) {
-  let placeholder = document.getElementById("placeholder");
-  placeholder.innerHTML = number;
+  
+
+  let diceImage = document.getElementById("dice-image");
+  const numberNames = ["one", "two", "three", "four", "five", "six"];
+  let imageName = "dice-six-faces-" + numberNames[number - 1] + ".png";
+  diceImage.src = "img/" + imageName;
 }
 
 const button = document.getElementById("button");
