f8b96993424905c4cc869f18160c02930f2a2624
FreeWifi0802 <104181615+FreeWifi0802@users.noreply.github.com>
2025-04-30 16:15:08 -0400

    Finished Lab

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
+let numberLookup = ["one", "two", "three", "four", "five", "six"];
+
 let dice = {
   sides: 6,
   roll: function () {
   let randomNumber;
   // Write Code Here
   // Generate a random number between 1 and the number of sides and assign it to randomNumber
-
-   return (randomNumber);
+  randomNumber = Math.floor(Math.random() * 6) + 1; 
+  return (randomNumber);
   }
 };
 
 function printNumber(number) {
   let placeholder = document.getElementById("placeholder");
-  placeholder.innerHTML = number;
+  placeholder.innerHTML = '<img src= /img/dice-six-faces-' + numberLookup[number - 1] + '.png width = 150>';
 }
 
 const button = document.getElementById("button");
