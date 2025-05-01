c31758098c74a278a72e53ae6c5b3b39b4e1ae04
Jiten0 <jitentamang929@gmail.com>
2025-05-01 16:49:36 -0400

    resolved main

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let dice = {
   sides: 6,
   roll: function () {
-  let randomNumber;
-  // Write Code Here
-  // Generate a random number between 1 and the number of sides and assign it to randomNumber
 
+  // Write Code Here
+  const randomNumber = getRandomIntInclusive(1, 6);
+  
+  // Generate a random number between 1 and the number of sides and assign it to randomNumber
+  
    return (randomNumber);
   }
 };
@@ -13,6 +15,14 @@ function printNumber(number) {
   let placeholder = document.getElementById("placeholder");
   placeholder.innerHTML = number;
 }
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+
+
 
 const button = document.getElementById("button");
 
