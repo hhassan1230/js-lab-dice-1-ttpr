952c0cdc09c9a46a4cf3b6c47a1bca8fc54351af
Noah Alayon <noahalayon01@gmail.com>
2025-05-01 01:08:41 -0400

    Implement random dice roll

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 let dice = {
   sides: 6,
   roll: function () {
-  let randomNumber;
-  // Write Code Here
-  // Generate a random number between 1 and the number of sides and assign it to randomNumber
+    let randomNumber;
+    randomNumber = Math.floor(Math.random() * this.sides) + 1
 
-   return (randomNumber);
+    return randomNumber;
   }
 };
 
